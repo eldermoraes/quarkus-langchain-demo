707c0c7e85d8e8711a77bc1ee993ec537b50520b
Elder Moraes <elder@eldermoraes.com>
2025-10-22 14:26:31 -0300

    Change from Open API remote model to a local gpt-oss using Ollama

--- quarkus-embedding-app/src/main/java/com/eldermoraes/embedding/BookingTools.java
+++ quarkus-embedding-app/src/main/java/com/eldermoraes/embedding/BookingTools.java
@@ -12,12 +12,12 @@ public class BookingTools {
         this.bookingService = bookingService;
     }
 
-    @Tool
+    @Tool("Get details about bookings")
     public Booking getBookingDetails(String bookingNumber, String customerName, String customerSurname) {
         return bookingService.getBookingDetails(bookingNumber, customerName, customerSurname);
     }
 
-    @Tool
+    @Tool("Cancel a booking based on the [bookingNumber], [customerName] and [customerSurname]")
     public void cancelBooking(String bookingNumber, String customerName, String customerSurname) {
         bookingService.cancelBooking(bookingNumber, customerName, customerSurname);
     }
